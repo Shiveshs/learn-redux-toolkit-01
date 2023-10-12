0ec867146adb9460340b18735ce86c1a04476cc6
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2023-10-12 21:05:49 +0530

    adding post slice

---
+++ src/projects/simplePost/simplePost.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const simplePost = () => {
+  return (
+    <div>simplePost</div>
+  )
+}
+
+export default simplePost
---
+++ src/projects/simplePost/simplePostSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const SimplepostSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {
+        addPost: (state, payload) => {}
+    }
+})
